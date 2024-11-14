1760b149fef61cb654e8db178a6805517f8598e6
Jaclyn Staples <jaclynonacloud@gmail.com>
2024-11-14 17:45:15 -0400

    Added a splash image and moved some styling to a root layout component.

--- src/routes/sender/+layout.svelte
+++ src/routes/sender/+layout.svelte
@@ -7,37 +7,6 @@
 </section>
 
 <style lang="scss">
-	:root {
-		--col-obj-background: black;
-		--col-obj-primary: rgb(39, 39, 39);
-		--col-obj-accent: rgb(39, 101, 143);
-		--col-obj-border: rgb(78, 78, 78);
-		--col-obj-error: rgb(122, 15, 15);
-		--col-obj-success: rgb(63, 226, 98);
-	}
-
-	:global(html, body) {
-		padding: 0;
-		margin: 0;
-		min-height: 100svh;
-		background: var(--col-obj-background);
-	}
-
-	:global(h1, h2, h3) {
-		padding: 0;
-		margin: 0;
-	}
-
-	:global(svg.control) {
-		width: 32px;
-		height: 32px;
-	}
-
-	:global(svg.header) {
-		width: 50px;
-		height: 50px;
-	}
-
 	.sender {
 		font-family: 'Quicksand', 'Arial', sans-serif;
 		max-width: 1200px;
